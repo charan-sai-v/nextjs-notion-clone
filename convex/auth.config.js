2a452a6cb4fe00409cbb89dd0b1fbe827332fb38
charan-sai-v <vcharansaiv@gmail.com>
2023-12-19 18:00:16 +0530

    fix a convex error

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://singular-griffon-56.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'files.edgestore.dev',
+        port: '',
+
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
